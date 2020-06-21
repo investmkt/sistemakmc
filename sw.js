3b4b7be8e8299844f9205498080bf3b065278aeb
Emerson <emersonrochadeoliveira@gmail.com>
2020-06-21 17:56:24 -0300

    arquivos manifest e sw

---
+++ sw.js
@@ -0,0 +1,11 @@
+// self.addEventListener('install', function(event) {
+//     event.waitUntil(
+//       caches.open('v1').then(function(cache) {
+//         return cache.addAll([         
+//             '/painel/index.html'
+//         ]);
+//       })
+//     );
+//   });
+
+self.addEventListener('fetch', function (event) {});
